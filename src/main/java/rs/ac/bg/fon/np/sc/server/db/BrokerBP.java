ea9ca127c4df680c91c4eb80ccfce35801f2e693
UrosVesic <uros99uki@gmail.com>
2022-04-21 00:31:06 +0200

    implementirana metoda zapamtiSlog

--- src/main/java/rs/ac/bg/fon/np/sc/server/db/BrokerBP.java
+++ src/main/java/rs/ac/bg/fon/np/sc/server/db/BrokerBP.java
@@ -6,6 +6,10 @@
 package rs.ac.bg.fon.np.sc.server.db;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import rs.ac.bg.fon.np.sc.commonLib.domen.OpstiDomenskiObjekat;
 
 /**
  *
@@ -31,6 +35,27 @@ public class BrokerBP {
         DbFabrikaKonekcije.getInstanca().getKonekcija().rollback();
     }
 
-   
+    public OpstiDomenskiObjekat zapamtiSlog(OpstiDomenskiObjekat odo) throws Exception {
+        String upit;
+        try {
+            upit = "INSERT INTO " + odo.vratiImeKlase()
+                    + " VALUES (" + odo.vratiVrednostiAtributa() + ")";
+            konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
+            Statement statement = konekcija.createStatement();
+            statement.executeUpdate(upit, Statement.RETURN_GENERATED_KEYS);
+            ResultSet rsId = statement.getGeneratedKeys();
+            if (rsId.next()) {
+                long id = rsId.getLong(1);
+                odo.postaviVrednostPK(id);
+            }
+            else{
+                throw new Exception("Neuspesno generisanje primarnog kljuca");
+            }
+            statement.close();
+        } catch (SQLException ex) {
+            throw ex;
+        }
+        return odo;
+    }
 
 }
