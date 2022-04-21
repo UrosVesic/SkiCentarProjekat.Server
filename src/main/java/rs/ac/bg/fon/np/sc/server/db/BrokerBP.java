d2c5fce9954b985471db0159c94e0d47ad9e6e57
UrosVesic <uros99uki@gmail.com>
2022-04-21 13:26:46 +0200

    implementirane sve CRUD operacije i metoda daLiPostojiSlog

--- src/main/java/rs/ac/bg/fon/np/sc/server/db/BrokerBP.java
+++ src/main/java/rs/ac/bg/fon/np/sc/server/db/BrokerBP.java
@@ -37,25 +37,82 @@ public class BrokerBP {
 
     public OpstiDomenskiObjekat zapamtiSlog(OpstiDomenskiObjekat odo) throws Exception {
         String upit;
-        try {
+        try (Statement statement = konekcija.createStatement();) {
             upit = "INSERT INTO " + odo.vratiImeKlase()
                     + " VALUES (" + odo.vratiVrednostiAtributa() + ")";
             konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
-            Statement statement = konekcija.createStatement();
             statement.executeUpdate(upit, Statement.RETURN_GENERATED_KEYS);
             ResultSet rsId = statement.getGeneratedKeys();
             if (rsId.next()) {
                 long id = rsId.getLong(1);
                 odo.postaviVrednostPK(id);
-            }
-            else{
+            } else {
                 throw new Exception("Neuspesno generisanje primarnog kljuca");
             }
-            statement.close();
         } catch (SQLException ex) {
             throw ex;
         }
         return odo;
+    }
+
+    public void promeniSlog(OpstiDomenskiObjekat odo) throws SQLException, Exception {
+        try (Statement statement = konekcija.createStatement()) {
+            String upit = "UPDATE " + odo.vratiImeKlase() + " SET " + odo.postaviVrednostiAtributa() + " WHERE " + odo.vratiUslovZaPromeniSlog();
+            konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
+            if (statement.executeUpdate(upit) == 0) {
+                throw new Exception("Ne postoji slog za promenu u bazi");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+    }
+
+    public void obrisiSlog(OpstiDomenskiObjekat odo) throws Exception {
+        try (Statement statement = konekcija.createStatement()) {
+            String upit = "DELETE FROM " + odo.vratiImeKlase() + " WHERE " + odo.vratiUslovZaNadjiSlog();
+            konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
+            if (statement.executeUpdate(upit) == 0) {
+                throw new Exception("Ne postoji slog za brisanje u bazi");
+            }
+        } catch (SQLException ex) {
+            throw ex;
+        }
+
+    }
+
+    public OpstiDomenskiObjekat pronadjiSlog(OpstiDomenskiObjekat odo) throws SQLException, Exception {
+        try (Statement statement = konekcija.createStatement()) {
+            String upit = "SELECT * FROM " + odo.vratiImeKlase() + " WHERE " + odo.vratiUslovZaNadjiSlog();
+            konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
+            ResultSet rs = statement.executeQuery(upit);
+            if (rs.next()) {
+                odo.napuni(rs);
+                for (int i = 0; i < odo.vratiBrojVezanihObjekata(); i++) {
+                    OpstiDomenskiObjekat vezo = odo.vratiVezaniObjekat(i);
+                    pronadjiSlog(vezo);
+                    odo.postaviVrednostVezanogObjekta(vezo, i);
+                }
+            } else {
+                throw new Exception("Slog nije pronadjen\n");
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
+        return odo;
+    }
+
+    public boolean daLiPostojiSlog(OpstiDomenskiObjekat odo) throws SQLException, Exception {
+        try (Statement statement = konekcija.createStatement()){
+            String upit = "SELECT * FROM " + odo.vratiImeKlase() + " WHERE " + odo.vratiUslovZaNadjiSlog();
+            konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
+            ResultSet rs = statement.executeQuery(upit);
+            return rs.next();
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
     }
 
 }
