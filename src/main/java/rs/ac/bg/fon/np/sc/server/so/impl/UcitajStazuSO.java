54fd011f7e06264fd516a29fb18082720e2afe5e
UrosVesic <uros99uki@gmail.com>
2022-04-23 12:31:15 +0200

    Dodate klase za sistemske operacije ucitaj i promeni stazu

---
+++ src/main/java/rs/ac/bg/fon/np/sc/server/so/impl/UcitajStazuSO.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.np.sc.server.so.impl;
+
+import rs.ac.bg.fon.np.sc.commonlib.domen.OpstiDomenskiObjekat;
+import rs.ac.bg.fon.np.sc.server.db.BrokerBP;
+import rs.ac.bg.fon.np.sc.server.so.OpstaSO;
+
+/**
+ *
+ * @author draskovesic
+ */
+public class UcitajStazuSO extends OpstaSO {
+
+    public UcitajStazuSO(BrokerBP b, OpstiDomenskiObjekat odo) {
+        super(b, odo);
+    }
+
+    @Override
+    public void izvrsiOperaciju() throws Exception {
+        b.pronadjiSlog(odo);
+    }
+
+    @Override
+    public void proveriPreduslove() throws Exception {
+    }
+
+}
