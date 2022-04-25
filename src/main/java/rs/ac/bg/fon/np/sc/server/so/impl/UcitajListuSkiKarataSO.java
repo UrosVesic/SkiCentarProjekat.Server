251b3e6311358bb4363ee9cd465eba0fb485e6a7
UrosVesic <uros99uki@gmail.com>
2022-04-25 19:00:29 +0200

    Dodate sistemske operacije ZapamtiSkiPasSO i UcitajListuSkiKarataSO

---
+++ src/main/java/rs/ac/bg/fon/np/sc/server/so/impl/UcitajListuSkiKarataSO.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.np.sc.server.so.impl;
+
+import java.util.List;
+import rs.ac.bg.fon.np.sc.commonlib.domen.OpstiDomenskiObjekat;
+import rs.ac.bg.fon.np.sc.server.db.BrokerBP;
+import rs.ac.bg.fon.np.sc.server.so.OpstaSO;
+
+/**
+ *
+ * @author UrosVesic
+ */
+public class UcitajListuSkiKarataSO extends OpstaSO {
+
+    public UcitajListuSkiKarataSO(BrokerBP b, OpstiDomenskiObjekat odo) {
+        super(b, odo);
+    }
+
+    @Override
+    public void izvrsiOperaciju() throws Exception {
+        lista = b.vratiSve(odo);
+    }
+
+    @Override
+    public void proveriPreduslove() throws Exception {
+    }
+
+}
