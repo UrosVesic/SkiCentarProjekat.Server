a7ae4f4e0be3f638fa3bbc672a18e48b717ba58b
UrosVesic <uros99uki@gmail.com>
2022-05-05 16:37:56 +0200

    Dodata klasa OpstaSOTest

---
+++ src/test/java/rs/ac/bg/fon/np/sc/server/so/OpstaSOTest.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.np.sc.server.so;
+
+import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import rs.ac.bg.fon.np.sc.server.db.BrokerBP;
+
+/**
+ *
+ * @author UrosVesic
+ */
+@ExtendWith(MockitoExtension.class)
+public abstract class OpstaSOTest {
+
+    @Mock
+    protected BrokerBP brokerBP;
+    protected OpstaSO testSO;
+
+    public abstract void testIzvrsiOperaciju() throws Exception;
+
+}
