f80df67c76bd8f202c57cfae52205b303409c63a
UrosVesic <uros99uki@gmail.com>
2022-05-21 19:31:02 +0200

    Dodata sistemska operacija zapamti kupca

---
+++ src/main/java/rs/ac/bg/fon/np/sc/server/so/impl/ZapamtiKupcaSO.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rs.ac.bg.fon.np.sc.server.so.impl;
+
+import rs.ac.bg.fon.np.sc.commonlib.domen.OpstiDomenskiObjekat;
+import rs.ac.bg.fon.np.sc.commonlib.validator.ValidationException;
+import rs.ac.bg.fon.np.sc.server.db.BrokerBP;
+import rs.ac.bg.fon.np.sc.server.so.OpstaSO;
+
+/**
+ *
+ * @author UrosVesic
+ */
+public class ZapamtiKupcaSO extends OpstaSO {
+
+    public ZapamtiKupcaSO(BrokerBP b, OpstiDomenskiObjekat odo) {
+        super(b, odo);
+    }
+
+    @Override
+    public void izvrsiOperaciju() throws Exception {
+        b.zapamtiSlog(odo);
+    }
+
+    @Override
+    protected void proveriPreduslove() throws Exception, ValidationException {
+    }
+
+}
