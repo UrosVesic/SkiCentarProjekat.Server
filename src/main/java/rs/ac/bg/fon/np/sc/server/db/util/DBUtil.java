59bbafbb6464fe924b9ed6d0322e1b9b8c299d2a
UrosVesic <uros99uki@gmail.com>
2022-05-23 00:33:53 +0200

    Dodat javaDoc

--- src/main/java/rs/ac/bg/fon/np/sc/server/db/util/DBUtil.java
+++ src/main/java/rs/ac/bg/fon/np/sc/server/db/util/DBUtil.java
@@ -11,55 +11,75 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
+ * Klasa koja cita i vraca parametre za konfiguraciju konekcije sa bazom
  *
  * @author UrosVesic
  */
-
-/*
- ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-            try (InputStreamReader in = new InputStreamReader(classloader.getResourceAsStream("server/db.json"))) {
-
-                Gson gson = new Gson();
-
-                JsonObject json = gson.fromJson(in, JsonObject.class);
-
-                String url = json.get("url").getAsString();
-                String user = json.get("username").getAsString();
-                String password = json.get("password").getAsString();
- */
 public class DBUtil {
 
-    private InputStreamReader in;
+    /**
+     * Iscitava konfiguracioni fajl db.json
+     */
+    private final InputStreamReader in;
+    /**
+     * Instanca klase DBUtil, implementacija Singleton paterna
+     */
     private static DBUtil instance;
-    private Gson gson;
-    private JsonObject json;
+    /**
+     * Gson objekat za rad sa json formatom
+     */
+    private final Gson gson;
+    /**
+     * JsonObject iščitan iz db.json fajla
+     */
+    private final JsonObject json;
 
+    /**
+     * Default konstrukot klase DBUtil. Otvara konfoguracioni fajl db.json, i
+     * inicijalizuje objekte gson i json.
+     */
     public DBUtil() {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         in = new InputStreamReader(classloader.getResourceAsStream("server/db.json"));
         gson = new Gson();
         json = gson.fromJson(in, JsonObject.class);
     }
-
-    public static DBUtil getInstance() throws IOException {
+    /**
+     * Staticka metoda koja kreira objekat klase DBUtil
+     * @return Postojecu instancu klase DBUtil ili novu ako ista ne postoji
+     */
+    public static DBUtil getInstance()  {
         if (instance == null) {
             instance = new DBUtil();
         }
         return instance;
     }
-
+    
+    /**
+     * Vraca parametar url za konekciju sa bazom
+     * @return Url parametar kao String
+     */
     public String getUrl() {
         return json.get("url").getAsString();
     }
-
+    /**
+     * Vraca parametar user za konekciju sa bazom
+     * @return User parametar kao String
+     */
     public String getUser() {
         return json.get("username").getAsString();
     }
-
+    /**
+     * Vraca parametar password za konekciju sa bazom
+     * @return Password parametar kao String
+     */
     public String getPassword() {
         return json.get("password").getAsString();
     }
-
+    /**
+     * Vraca parametar Connection factory class name za konekciju sa bazom
+     * @return Connection factory class name parametar kao String
+     */
     public String getConnectionFactoryClassName() {
         return json.get("connection_factory_class_name").getAsString();
     }
