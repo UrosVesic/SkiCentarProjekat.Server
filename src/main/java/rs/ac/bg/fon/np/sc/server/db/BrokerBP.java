7c393d3609fb915af2a64e27e87dabc1d4f28017
UrosVesic <uros99uki@gmail.com>
2022-05-23 00:14:00 +0200

    Dodata metoda vratiMaxRbSlabogObjekta(OpstiDomenskiObjeakt)
    koja vraca maksimalnu vrednost nenasledjenog dela primarnog kompozitnog kljuca slabog objekta

--- src/main/java/rs/ac/bg/fon/np/sc/server/db/BrokerBP.java
+++ src/main/java/rs/ac/bg/fon/np/sc/server/db/BrokerBP.java
@@ -16,47 +16,59 @@ import rs.ac.bg.fon.np.sc.commonlib.domen.OpstiDomenskiObjekat;
 
 /**
  * Klasa zaduzena za interakciju sa bazom podataka i izvrsavanje CRUD operacija
+ *
  * @author UrosVesic
  */
 public class BrokerBP {
-    
+
     /**
      * Konekcija sa bazom podataka
      */
     private Connection konekcija;
+
     /**
      * Uspostavlja konekciju sa bazom podataka, instancira objekat konekcija.
-     * @throws Exception  ako nije moguce uspostaviti konekciju sa bazom podataka
+     *
+     * @throws Exception ako nije moguce uspostaviti konekciju sa bazom podataka
      */
     public void uspostaviKonekciju() throws Exception {
         DbFabrikaKonekcije.getInstanca().getKonekcija();
     }
+
     /**
      * Raskida konekciju sa bazom podataka
+     *
      * @throws Exception ako nije moguce zatvoriti konekciju sa bazom podataka
      */
     public void raskiniKonekciju() throws Exception {
         DbFabrikaKonekcije.getInstanca().getKonekcija().close();
     }
+
     /**
      * Potvrdjuje transakciju
+     *
      * @throws Exception ako nije moguce potvrditi transakciju
      */
     public void potvrdiTransakciju() throws Exception {
         DbFabrikaKonekcije.getInstanca().getKonekcija().commit();
     }
+
     /**
      * Ponistava transakciju
+     *
      * @throws Exception ako nije moguce ponistiti transakciju
      */
     public void ponistiTransakciju() throws Exception {
         DbFabrikaKonekcije.getInstanca().getKonekcija().rollback();
     }
-    
+
     /**
      * Cuva domenski objekat kao slog u bazi podatala
+     *
      * @param odo domenski objekat koji treba sacuvati
-     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja konekcije ili kreiranja SQL statement-a, ili primarni kljuc nije uspesno generisan
+     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja
+     * konekcije ili kreiranja SQL statement-a, ili primarni kljuc nije uspesno
+     * generisan
      */
     public void zapamtiSlog(OpstiDomenskiObjekat odo) throws Exception {
         String upit;
@@ -76,10 +88,15 @@ public class BrokerBP {
             throw ex;
         }
     }
+
     /**
      * Menja slog u bazi podataka sa podacima iz domenskog objekta
-     * @param odo objekat koji sadrzi primarni kljuc sloga koji treba promeniti i ostale podatke koje treba upisati u slog
-     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja konekcije ili kreiranja SQL statement-a, ili ne postoji zadati slog za promenu
+     *
+     * @param odo objekat koji sadrzi primarni kljuc sloga koji treba promeniti
+     * i ostale podatke koje treba upisati u slog
+     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja
+     * konekcije ili kreiranja SQL statement-a, ili ne postoji zadati slog za
+     * promenu
      */
     public void promeniSlog(OpstiDomenskiObjekat odo) throws Exception {
         konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
@@ -93,10 +110,14 @@ public class BrokerBP {
             throw ex;
         }
     }
+
     /**
      * Brise slog sa primarnim kljucem koji je postavljen u okviru objekta odo
+     *
      * @param odo objekat koji u sebi nosi primarni kljuc za brisanje sloga
-     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja konekcije ili kreiranja SQL statement-a, ili slog sa zadatim primarnim kljucem ne postoji
+     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja
+     * konekcije ili kreiranja SQL statement-a, ili slog sa zadatim primarnim
+     * kljucem ne postoji
      */
     public void obrisiSlog(OpstiDomenskiObjekat odo) throws Exception {
         konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
@@ -110,12 +131,15 @@ public class BrokerBP {
         }
 
     }
+
     /**
      * Pronalazi slog u bazi po primarnom kljucu
+     *
      * @param odo objekat koji u sebi nosi primarni kljuc za pronalazenje sloga
-     * @return pronadjeni domenski objekat ili
-     *         null ako ne postoji domenski objekat
-     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja konekcije ili kreiranja SQL statement-a, ili ako nije moguce pronaci slog
+     * @return pronadjeni domenski objekat ili null ako ne postoji domenski
+     * objekat
+     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja
+     * konekcije ili kreiranja SQL statement-a, ili ako nije moguce pronaci slog
      */
     public OpstiDomenskiObjekat pronadjiSlogPoKljucu(OpstiDomenskiObjekat odo) throws Exception {
         OpstiDomenskiObjekat pronadjeni = null;
@@ -138,12 +162,16 @@ public class BrokerBP {
         }
         return pronadjeni;
     }
-    
+
     /**
      * Proverava postojanje sloga u bazi sa zadatim primarnim kljucem
-     * @param odo domenski objekat koji u sebi nosi primarni kljuc za pronalazenje sloga
-     * @return true ako slog postoji ili false ukoliko slog sa zadatim kljucem ne postoji u bazi
-     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja konekcije ili kreiranja SQL statement-a
+     *
+     * @param odo domenski objekat koji u sebi nosi primarni kljuc za
+     * pronalazenje sloga
+     * @return true ako slog postoji ili false ukoliko slog sa zadatim kljucem
+     * ne postoji u bazi
+     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja
+     * konekcije ili kreiranja SQL statement-a
      */
     public boolean daLiPostojiSlog(OpstiDomenskiObjekat odo) throws Exception {
         konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
@@ -156,11 +184,16 @@ public class BrokerBP {
             throw ex;
         }
     }
+
     /**
      * Vraca sve slogove iz baze koji odgovaraju tipu parametra odo
+     *
      * @param odo parametar koji odredjuje iz koje ce se tabele uzimati slogovi
-     * @return listu svih objekata izvucenih iz baze podataka ili praznu listu ukoliko ne postoji domenski objekat
-     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja konekcije, kreiranja SQL statement-a,izvrsavanja upita ili nije moguce pronaci neki od vezanih objekata u bazi
+     * @return listu svih objekata izvucenih iz baze podataka ili praznu listu
+     * ukoliko ne postoji domenski objekat
+     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja
+     * konekcije, kreiranja SQL statement-a,izvrsavanja upita ili nije moguce
+     * pronaci neki od vezanih objekata u bazi
      */
     public List<OpstiDomenskiObjekat> vratiSve(OpstiDomenskiObjekat odo) throws Exception {
         List<OpstiDomenskiObjekat> lista = new ArrayList<>();
@@ -185,11 +218,17 @@ public class BrokerBP {
         }
         return lista;
     }
+
     /**
      * Pronalazi slogove iz baze koji ispunjavaju zadati kriterijum
-     * @param odo domenski objekat koji u sebi sadrzi podatke za sastavljanje uslova za pretragu
-     * @return listu pronadjenih domenskih objekata ili praznu listu ukoliko nijedan slog ne ispunajva zadati uslov
-     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja konekcije, kreiranja SQL statement-a,izvrsavanja upita ili nije moguce pronaci neki od vezanih objekata u bazi
+     *
+     * @param odo domenski objekat koji u sebi sadrzi podatke za sastavljanje
+     * uslova za pretragu
+     * @return listu pronadjenih domenskih objekata ili praznu listu ukoliko
+     * nijedan slog ne ispunajva zadati uslov
+     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja
+     * konekcije, kreiranja SQL statement-a,izvrsavanja upita ili nije moguce
+     * pronaci neki od vezanih objekata u bazi
      */
     public List<OpstiDomenskiObjekat> pronadjiSlogove(OpstiDomenskiObjekat odo) throws Exception {
         List<OpstiDomenskiObjekat> lista = new ArrayList<>();
@@ -213,11 +252,19 @@ public class BrokerBP {
         }
         return lista;
     }
+
     /**
-     * Pronalazi slogove iz baze po vrednosti unique kolone, ukoliko prosledjena vrednost nije unique i postoji vise slogova koji ispunajvaju uslov, metoda vraca samo prvi objekat
-     * @param odo domenski objekat koji u sebi sadrzi podatke za sastavljanje uslova za pretragu
-     * @return pronadjeni domenski objekat ili null ukoliko domenski objekat nije pronadjen
-     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja konekcije, kreiranja SQL statement-a,izvrsavanja upita ili nije moguce pronaci neki od vezanih objekata u bazi
+     * Pronalazi slogove iz baze po vrednosti unique kolone, ukoliko prosledjena
+     * vrednost nije unique i postoji vise slogova koji ispunajvaju uslov,
+     * metoda vraca samo prvi objekat
+     *
+     * @param odo domenski objekat koji u sebi sadrzi podatke za sastavljanje
+     * uslova za pretragu
+     * @return pronadjeni domenski objekat ili null ukoliko domenski objekat
+     * nije pronadjen
+     * @throws Exception ukoliko dodje do greske prilikom uspostavljanja
+     * konekcije, kreiranja SQL statement-a,izvrsavanja upita ili nije moguce
+     * pronaci neki od vezanih objekata u bazi
      */
     public OpstiDomenskiObjekat pronadjiSlogUnique(OpstiDomenskiObjekat odo) throws Exception {
         OpstiDomenskiObjekat pronadjeni = null;
@@ -239,6 +286,24 @@ public class BrokerBP {
             throw ex;
         }
         return pronadjeni;
+    }
+
+    public long vratiMaxRbSlabogObjekta(OpstiDomenskiObjekat odo) throws Exception {
+        try {
+            String upit;
+            upit = "SELECT Max(" + odo.vratiNazivPK() + ") AS " + odo.vratiNazivPK() + " FROM " + odo.vratiImeTabeleZaKlasu() + " WHERE " + odo.vratiUslovZaNadjiSlogove();
+            Connection konekcija = DbFabrikaKonekcije.getInstanca().getKonekcija();
+            Statement statement = konekcija.createStatement();
+            ResultSet rs = statement.executeQuery(upit);
+            if (rs.next() == false) {
+                return 0;
+            } else {
+                return rs.getLong(odo.vratiNazivPK());
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            throw ex;
+        }
     }
 
 }
