cff882b3763845db192b259f1dda3caba07768a5
UrosVesic <uros99uki@gmail.com>
2022-05-22 19:04:23 +0200

    Dodat atribut ConnectionPool pool i promenjena metoda getKonekcija

--- src/main/java/rs/ac/bg/fon/np/sc/server/db/DbFabrikaKonekcije.java
+++ src/main/java/rs/ac/bg/fon/np/sc/server/db/DbFabrikaKonekcije.java
@@ -5,73 +5,71 @@
  */
 package rs.ac.bg.fon.np.sc.server.db;
 
-import com.google.gson.Gson;
-import com.google.gson.JsonObject;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
+import rs.ac.bg.fon.np.sc.server.db.util.DBUtil;
 
 /**
  * Singleton klasa koja je zaduzena za kreiranje konekcije sa bazom podataka
+ *
  * @author UrosVesic
  */
 public class DbFabrikaKonekcije {
-    
+
+    private ConnectionPool pool;
+
     /**
      * Konekcija sa bazom podataka
      */
     private Connection konekcija;
     /**
-     * Instanca klase DbFarbrikaKonekcije 
+     * Instanca klase DbFarbrikaKonekcije
      */
     private static DbFabrikaKonekcije instanca;
-    
+
     /**
      * Default konstruktor koji postavlja atribute na predefinisane vrednosti
      */
-    private DbFabrikaKonekcije() {
+    private DbFabrikaKonekcije() throws Exception {
+        try {
+            pool = new ConnectionPool();
+            pool.inicijalizacijaConnectionPoolDataSource();
+        } catch (Exception ex) {
+            throw new Exception("Neuspensa inicijalizacija connection pool data source-a");
+        }
     }
-    
+
     /**
-     * Pravi novu ili vraca vec postojecu instancu klase DbFarbrikaKonekcije. Predstavlja implementaciju Singleton paterna
+     * Pravi novu ili vraca vec postojecu instancu klase DbFarbrikaKonekcije.
+     * Predstavlja implementaciju Singleton paterna
+     *
      * @return instancu klase DbFabrikaKonekcije
+     * @throws Exception
      */
-    public static DbFabrikaKonekcije getInstanca() {
+    public static DbFabrikaKonekcije getInstanca() throws Exception {
         if (instanca == null) {
             instanca = new DbFabrikaKonekcije();
         }
         return instanca;
     }
-    
+
     /**
      * Vraca novu konekciju sa bazom podataka
+     *
      * @return konekciju sa bazom podataka kao objekat klase Connection
      * @throws Exception ako nije moguce uspostaviti konekciju sa bazom podataka
      */
     public Connection getKonekcija() throws Exception {
 
         if (konekcija == null || konekcija.isClosed()) {
-            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
-            try (InputStreamReader in = new InputStreamReader(classloader.getResourceAsStream("server/db.json"))) {
-
-                Gson gson = new Gson();
-
-                JsonObject json = gson.fromJson(in, JsonObject.class);
-
-                String url = json.get("url").getAsString();
-                String user = json.get("username").getAsString();
-                String password = json.get("password").getAsString();
-
-                konekcija = DriverManager.getConnection(url, user, password);
+            try {
+                konekcija = pool.uspostaviKonekciju();
                 konekcija.setAutoCommit(false);
             } catch (SQLException ex) {
                 System.out.println("Neuspesno uspostavljanje konekcije!\n" + ex.getMessage());
                 throw ex;
-            } catch (IOException ex) {
-                System.out.println("Neuspesno citanje konfuguracije iz fajla");
-                throw ex;
+
             }
         }
         return konekcija;
